6e1ca39f48cacfeef691452a818670562c13c423
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-10-13 11:34:10 +0300

    adding questions model and list

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,15 @@
+
+
+class QuizQuestion {
+  final String questionText;
+  final List<String> choices;
+  // final List<int> choicesScore;
+  // final List<MaterialColor> buttonsColor;
+
+  const QuizQuestion({
+    required this.questionText,
+    required this.choices,
+    // required this.choicesScore,
+    // this.buttonsColor = const [],
+  });
+}
